aca1023a6261b59f4a425314dd6913ae5f0cd5d5
João Pedro Ribeiro Cunha <joaopedrorbcunha@gmail.com>
2026-06-04 22:40:33 -0300

    feat: Adicionando código de programação das aulas 4 e 5

---
+++ aulas/aula-04-tipos-basicos-e-variaveis/codigo/aula04_variaveis_operadores_expressoes.py
@@ -0,0 +1,36 @@
+print("VARIÁVEIS E TIPOS BÁSICOS:")
+nome = "João"
+idade = 20
+altura = 1.75
+ativo = True
+
+print("Nome:", nome)
+print("Idade:", idade)
+print("Altura:", altura)
+print("Ativo:", ativo)
+
+print("\nTipo do Nome:", type(nome))
+print("Tipo da Idade:", type(idade))
+print("Tipo da Altura:", type(altura))
+print("Tipo do Ativo:", type(ativo))
+
+print("\nOPERADORES ARITMÉTICOS:")
+a = 10
+b = 3
+print("Utilizando as variáveis a e b:")
+print("Soma:", a + b)
+print("Subtração:", a - b)
+print("Multiplicação:", a * b)
+print("Divisão:", a / b)
+print("Divisão Inteira:", a // b)
+print("Exponenciação:", a ** b)
+
+
+print("\nOPERADORES RELACIONAIS:")
+print("Utilizando as variáveis a e b:")
+print("Igualdade:", a == b)
+print("Desigualdade:", a != b)
+print("Maior que:", a > b)
+print("Menor que:", a < b)
+print("Maior ou igual:", a >= b)
+print("Menor ou igual:", a <= b)
